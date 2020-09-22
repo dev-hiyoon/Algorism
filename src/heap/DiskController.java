b04d74fa2b7782eedf6d69b93c7c8cd5f5d1bfa6
11102 <dev.hiyoon@gmail.com>
2020-09-22 17:18:54 +0900

    80% 확률 -> 근데 테스트케이스가 이상하긴 함

--- src/heap/DiskController.java
+++ src/heap/DiskController.java
@@ -1,15 +1,14 @@
 package heap;
 
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.PriorityQueue;
 
 public class DiskController {
     public static void main(String[] args) {
         // [[0, 3], [1, 9], [2, 6]]	9
         // A → B → C: 10ms(= (3 + 11 + 16) / 3)
         // A → C → B: 9ms(= (3 + 7 + 17) / 3)
-        int[][] jobs = new int[][]{{0, 3}, {1, 9}, {2, 6}, {4, 3}};
+        int[][] jobs = new int[][]{{0, 4}};
         int result = solution(jobs);
         System.out.println(result);
     }
@@ -21,34 +20,36 @@ public class DiskController {
         Arrays.sort(jobs, (o1, o2) -> o1[0] - o2[0]);
         answer = jobs[0][1];
         lastTime = jobs[0][0] + jobs[0][1];
-        int[][] temp = null;
-        while (idx < jobs.length) {
-            int maxIdx = 0;
-            for (int i = idx; i < jobs.length; i++) {
-                if (jobs[i][0] > lastTime) {
-                    break;
-                } else {
-                    maxIdx++;
-                }
+        printLog(jobs[0]);
+        PriorityQueue<int[]> queue = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+        while (true) {
+
+            while (idx < jobs.length && jobs[idx][0] <= lastTime) {
+                queue.add(jobs[idx++]);
             }
 
-            if (maxIdx > 0) {
-                temp = Arrays.copyOfRange(jobs, idx, idx + maxIdx);
-                Arrays.sort(temp, (o1, o2) -> o1[1] - o2[1]);
-                for (int i = 0; i < temp.length; i++) {
-                    answer += (lastTime - temp[i][0] + temp[i][1]);
-                    lastTime = (lastTime + temp[i][1]);
-                    break;
-                }
-
-                idx++;
-            } else {
+            if (queue.isEmpty() && idx < jobs.length) {
                 answer += jobs[idx][1];
                 lastTime = jobs[idx][0] + jobs[idx][1];
+                printLog(jobs[idx]);
                 idx++;
+            } else if (!queue.isEmpty()) {
+                int[] temp = queue.poll();
+                answer += (lastTime - temp[0] + temp[1]);
+                lastTime = (lastTime + temp[1]);
+                printLog(temp);
+            }
+
+            if (queue.isEmpty() && idx >= jobs.length) {
+                break;
             }
         }
 
         return answer / jobs.length;
     }
+
+    private static void printLog(int[] job) {
+        System.out.println(String.format("###### {%s, %s}", job[0], job[1]));
+    }
+
 }
