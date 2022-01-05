bc1ac2708370ef3e721237c64616d298d479c773
hiyoon <dev.hiyoon@gmail.com>
2022-01-05 18:55:22 +0900

    2021-12-05  - string d 추가

--- src/test/Test.java
+++ src/test/Test.java
@@ -10,6 +10,7 @@ public class Test {
         String a = "a";
         String b = "b";
         String c = "c";
+        String d = "d";
         /**
          * 선행 스킬이란 어떤 스킬을 배우기 전에 먼저 배워야 하는 스킬을 뜻합니다.
          *
